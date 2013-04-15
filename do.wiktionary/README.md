3a2eec2249e2315e6e4a5334f13b0aad1f5efdbc
Daehee Han <handol@gmail.com>
2013-04-15 10:38:11 +0000

    image url

--- do.wiktionary/README.md
+++ do.wiktionary/README.md
@@ -9,3 +9,20 @@
  * python wikxml2dict.py enwiktionary.500k.xml.bz2 > enwiktionary.500k.dict
 
 ## Build index file of dict file
+
+
+## image url
+* https://en.wikipedia.org/w/api.php
+* http://www.mediawiki.org/wiki/API:Properties
+* http://commons.wikimedia.org/wiki/FAQ#What_are_the_strangely_named_components_in_file_paths.3F
+* http://www.md5.cz/ 
+* http://en.wiktionary.org/wiki/File:Lion_waiting_in_Namibia.jpg
+* http://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Lion_waiting_in_Namibia.jpg/800px-Lion_waiting_in_Namibia.jpg
+
+
+## audio url
+* {{audio|En-uk-eschatology.ogg|Audio (UK)}}
+* <source src="//upload.wikimedia.org/wikipedia/commons/3/3e/En-uk-eschatology.ogg" type="audio/ogg; codecs=&quot;vorbis&quot;" data-title="Original Ogg file (411 kbps)" data-shorttitle="Ogg source" data-width="0" data-height="0" data-bandwidth="411402"></source>
+* http://en.wiktionary.org/wiki/File:En-uk-eschatology.ogg
+* http://upload.wikimedia.org/wikipedia/commons/3/3e/En-uk-eschatology.ogg
+
--- do.wiktionary/doit.sh
+++ do.wiktionary/doit.sh
@@ -2,7 +2,6 @@
 #
 python gzcat_part.py 5000 /home/search/disk2/wiki_visit_count/wiktionary/enwiktionary-20130313-abstract.xml.gz enwiktionary-20130313-abstract.xml.5000
 
-python gzcat_part.py 20000 /home/search/disk2/wiki_visit_count/wiktionary/enwiktionary-20130313-pages-meta-current.xml.bz2 enwiktionary-20130313-pages-meta-current.20k.xml
 
 
 python wiktio2xml.py enwiktionary-20130313-pages-meta-current.20k.xml a -q > b
@@ -11,3 +10,19 @@ python wikxml2dict.py http://dumps.wikimedia.org/enwiktionary/20130313/enwiktion
 python wikxml2dict.py http://dumps.wikimedia.org/enwiktionary/20130313/enwiktionary-20130313-imagelinks.sql.gz > b
 python wikxml2dict.py http://dumps.wikimedia.org/enwiktionary/20130313/enwiktionary-20130313-pages-meta-current.xml.bz2 > c
 python wikxml2dict.py http://dumps.wikimedia.org/enwiktionary/20130313/enwiktionary-20130313-pages-articles-multistream-index.txt.bz2 > d
+
+
+# full
+python wikxml2dict.py /home/search/disk2/wiki_visit_count/wiktionary/enwiktionary-20130313-pages-meta-current.xml.bz2 | bzip2 -c > /home/search/disk2/wiki_visit_count/wiktionary/good.bz2
+
+# partial
+python gzcat_part.py 100000 /home/search/disk2/wiki_visit_count/wiktionary/enwiktionary-20130313-pages-meta-current.xml.bz2 enwiktionary.100k.xml
+python wikxml2dict.py enwiktionary.100k.xml > enwiktionary.dict
+
+
+# image url
+# ex) Image:Albert Einstein Head.jpg
+wget http://en.wikipedia.org/w/api.php\?action=query\&titles=Image:Albert%20Einstein%20Head.jpg\&prop=imageinfo\&iiprop=url\&iiurlwidth=200
+curl http://en.wikipedia.org/w/api.php\?action=query\&titles=Image:Albert%20Einstein%20Head.jpg\&prop=imageinfo\&iiprop=url\&iiurlwidth=200\&format=json -o imgThumb.2.txt
+curl http://en.wikipedia.org/w/api.php\?action=query\&titles=File:Albert%20Einstein%20Head.jpg\&prop=imageinfo\&iiprop=url\&iiurlwidth=200\&format=json -o imgThumb.3.txt
+
