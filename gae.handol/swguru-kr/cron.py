03563d668ac0fa6b2293de9493a43beb65d752e9
daehee <handol@gmail.com>
2013-05-31 08:45:51 +0900

    cron urlfetch db

--- gae.handol/swguru-kr/cron.py
+++ gae.handol/swguru-kr/cron.py
@@ -6,6 +6,9 @@ import datetime
 import json
 import urllib2
 import urllib
+import gzip
+import logging
+from StringIO import StringIO
 
 class Mesg(db.Model):
 	mesg = db.StringProperty()
@@ -13,10 +16,10 @@ class Mesg(db.Model):
 
 class StackTags(db.Model):
 	tag = db.StringProperty(indexed=True)
-	#count = db.IntergerProperty(int)
-	#date = db.DateProperty(datetime.date, auto_now_add=True)
-	count = db.ListProperty(int, default=[])
-	date = db.ListProperty(datetime.date, default=[])
+	count = db.IntegerProperty(int)
+	date = db.DateProperty(datetime.date, auto_now_add=True)
+	#count = db.ListProperty(int, default=[])
+	#date = db.ListProperty(datetime.date, default=[])
 
 def get_stack_tags(i, gae=True):
 	url = 'http://api.stackoverflow.com/1.1/tags?page=%d&pagesize=100' % (i)
@@ -24,25 +27,31 @@ def get_stack_tags(i, gae=True):
 		result = urlfetch.fetch(url=url,
 			method=urlfetch.GET,
 			headers={'Content-Type': 'application/x-www-form-urlencoded'})
+		logging.info('urlfetch RESULT %d from %s' % (result.status_code, url))
+		result = result.content
 	else:
 		result = urllib2.urlopen(url)
-		res = result.read()
-		from gzip import GzipFile
-		result = GzipFile('', 'r', 0, StringIO(res)).read()
-		#print result
+		result = result.read()
+		result = gzip.GzipFile('', 'r', 0, StringIO(result)).read()
+	logging.info(result[:250])
+	#print result
 	res = json.loads(result)
 	taglist = res['tags']
 	for tag in taglist:
 		name = tag['name']
 		count = tag['count']
-		one = StackTags.get_or_insert(name)
-		one.count.append(count)
-		one.date.append(datetime.date.today())
+		one = StackTags()
+		one.tag = name
+		one.count = count
+		one.date = datetime.date.today()
+
+		one.put()
 
 class MyCron(webapp.RequestHandler):
 	def get(self):
 		for i in range(10):
 			get_stack_tags(i)
+			break
 		m = Mesg()
 		m.mesg = 'my cron haha'
 		m.put()
